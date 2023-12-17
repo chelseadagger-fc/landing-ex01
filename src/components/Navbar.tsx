06395d13310a05a9524538aa1b793d9aa20e0259
da.aldridge <da.aldridge@proton.me>
2023-12-16 22:56:42 -0500

    added css mouseover animations for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
+import './Navbar.css'
+
 export default function Navbar() {
     return (
         <div className="h-1/5 w-full flex-col justify-center items-center content-center pt-6">
-            <h1 className="text-6xl">WEBSITE</h1>          
+            <h1 className="text-6xl">WELCOME</h1>          
             <div className="w-full flex flex-row w-1/2 justify-center items-center content-center mt-4 gap-8">
-                <p className="text-3xl">[ HOME ]</p>
-                <p className="text-3xl">[ NAV2 ]</p>
-                <p className="text-3xl">[ NAV3 ]</p>
-                <p className="text-3xl">[ NAV4 ]</p>
+                <p className="text-3xl font-bold navigation">[ HOME ]</p>
+                <p className="text-3xl font-bold navigation">[ NEWS ]</p>
+                <p className="text-3xl font-bold navigation">[ FORUM ]</p>
+                <p className="text-3xl font-bold navigation">[ NAV4 ]</p>
+                <p className="text-3xl font-bold navigation">[ NAV5 ]</p>
             </div>
         </div>
     )
